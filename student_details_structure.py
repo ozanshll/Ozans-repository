a96d091c36e193e2223305f2586c95c31768d505
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-03-16 16:02:18 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,13 @@
+# String variables
+first_name = "Ozan "  
+last_name = "Sahilliogullari"  
+nationality = "Turkish"  
+
+# Integer 
+index_number = 35441  
+
+# String 
+starting_date = "2025-03-16"  
+
+# List variable 
+courses = ["Object Oriented Programming", "Data Structures", "Algorithms"]
